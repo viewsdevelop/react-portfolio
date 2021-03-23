74ba3bb5413f7eda0e2229f4ddb0d310968a8694
viewsdevelop <viewsdevelop@gmail.com>
2021-03-22 22:29:09 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello!</h1>
+                <p>My name is Louis. I'm a Front End Software Engineer.</p>
+                <p>I'm always looking forward to working on meaningful projects.</p>
+            </div>
+        )
+    }
+}
+
+export default App;
